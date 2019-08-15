022b41ed00e18abcdbe1d7a0bd24b55eb500dfa8
Jongmin Kim <jongmin.kim@mynrma.com.au>
2019-08-16 06:25:47 +1000

    WIP

--- src/FindRegoByJobNumber.js
+++ src/FindRegoByJobNumber.js
@@ -1,58 +1,54 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
-class MemberDetailsRow extends Component {
-    render() {
-      const memberDetail = this.props.memberDetail;
-      const name = memberDetail.rego;
-  
-      return (
-        <tr>
-          <td>{name}</td>
-          <td>{memberDetail.memberNumber}</td>
-        </tr>
-      );
-    }
-  }
-
-class MemberDetailsTable extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          details: this.props.memberDetail
-        };
-        this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-        this.handleInStockChange = this.handleInStockChange.bind(this);
-    }
-    
-  handleFilterTextChange(filterText) {
-    this.setState({
-      filterText: filterText
-    });
-  }
-  
-  handleInStockChange(inStockOnly) {
-    this.setState({
-      inStockOnly: inStockOnly
-    })
-  }
-
-    render() {
-       const rows = [];
-       console.log(this.props.memberDetail);
-
-        return (<table>
-            <thead>
-              <tr>
-                <th>Rego</th>
-                <th>Member Number</th>
-              </tr>
-            </thead>
-            <tbody>{rows}</tbody>
-          </table>
-        );
-    }
-}
+const batteryLocation = [
+  { 
+    "id": "1",
+    "totaltime" : "20 Mins",
+    "totalkm" : "4 Km",
+    "vehicle":
+      {
+      "SP_ID": "1234",
+      "ADDRESS": "2 GEORGE ST, PARRAMATTA, 2150",
+      "TIME": "20 Mins",
+      "DISTANCE": "4 Km"
+      },
+    "hub":
+      {
+      "SP_ID": "",
+      "ADDRESS": "",
+      "TIME": "",
+      "DISTANCE": ""
+      },
+    "customer":
+      {
+      "ADDRESS": "1 HAPPY STREET, SYDNEY, 2000",
+      }   
+    },
+    {
+      "id": "2",
+      "totaltime" : "54 Mins",
+      "totalkm" : "11 Km",
+      "vehicle":
+        {
+        "SP_ID": "1235",
+        "ADDRESS": "21 GEORGE ST, AUBURN, 2170",
+        "TIME": "20 Mins",
+        "DISTANCE": "4 Km"
+        },
+      "hub":
+        {
+        "SP_ID": "1234",
+        "ADDRESS": "3 GOOD ST, REDFERN, 2150",
+        "TIME": "34 Mins",
+        "DISTANCE": "7 Km"
+        },
+      "customer":
+        {
+        "ADDRESS": "1 HAPPY STREET, SYDNEY, 2000",
+        } 
+      },
+  ];
 
 class OnJobNumberSubmit extends Component {
     constructor(props) {
@@ -82,21 +78,49 @@ class OnJobNumberSubmit extends Component {
             "jobNumber": this.state.value            
         }
 
-    axios.post('https://6t7re99ysc.execute-api.ap-southeast-2.amazonaws.com/dev/V1/battery/job', data, header)
-    .then((result) => {
-        this.setState({
-            isLoaded: true,
-            details: result.data
-        });
+      axios.post('https://6t7re99ysc.execute-api.ap-southeast-2.amazonaws.com/dev/V1/battery/job', data, header)
+      .then((result) => {
+          this.setState({
+              isLoaded: true,
+              details: result.data
+          });
+          console.log(this.state.details);
+          this.handleSuccess(result);
+      })
+      .catch((error) => {
+          this.setState({
+              isLoaded: true,
+              error
+          });
+      });
+    }
 
-        console.log(this.state.details);
-    })
-    .catch((error) => {
-        this.setState({
-            isLoaded: true,
-            error
+    handleSuccess(result) {
+      if(result.data != null) {
+        const header = {          
+          'x-api-key': "ZT7muC1s485C5DtrgEloN8F0DVTm1M7JiRQQYUph",
+          'Access-Control-Allow-Origin': "*",
+          'Content-Type': 'application/json'
+          };
+
+        const data = {
+          "rego": this.state.value            
+        }
+
+        axios.post('https://6t7re99ysc.execute-api.ap-southeast-2.amazonaws.com/dev/V1/battery/rego', data, header)
+        .then((result) => {
+            this.setState({
+              compatibleBatteries: result.data.GetBatteriesResult,
+            });
+            console.log(this.state.compatibleBatteries);
+        })
+        .catch((error) => {
+            this.setState({
+                isLoaded: true,
+                error
+            });
         });
-    });
+      }
     }
   
     render() {
@@ -119,22 +143,22 @@ class OnJobNumberSubmit extends Component {
   }
 
 export default class FindRegoByJobNumber extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          error: null,
-          isLoaded: false,
-          value: null,
-          details: {},
-          compatibleBatteries: []
-        };
-      }
-      render() {        
-          return (
-              <div>
-                <OnJobNumberSubmit />
-                <MemberDetailsTable memberDetail={this.state.details}/>
-              </div>
-            );
-        }
+  constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        value: null,
+        details: {},
+        compatibleBatteries: []
+      };
+    }
+
+    render() {        
+        return (
+            <div>
+              <OnJobNumberSubmit />
+            </div>
+          );
+    }
 }
